bde2d4492e9c76942a0a6980ad5576144d2d152f
Samvar Jain <146375953+Samvar-Jain@users.noreply.github.com>
2025-08-28 04:17:56 +0530

    Delete src directory

--- src/routes/ai.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const aiControler = require('../controllers/ai.controller');
-const router = express.Router();
-
-router.post("/get-review", aiControler.getReview);
-
-
-module.exports = router;
